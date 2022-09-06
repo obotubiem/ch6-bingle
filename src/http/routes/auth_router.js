9ca1a3062e59a8464adb43e7675cb9a2bc60d169
irvan taufik <irvantaufik28@gmail.com>
2022-09-06 11:02:24 +0700

    error register

--- src/http/routes/auth_router.js
+++ src/http/routes/auth_router.js
@@ -1,8 +1,10 @@
 const express =require('express')
 const router = express.Router()
 const auth = require('../../controllers/auth_controller')
+const handleUpload =require('../../libs/handle_Upload')
 
-router.post('/login', auth.login)
+router.post('/login',handleUpload.single('photo_profile'), auth.login)
+router.post('/register', auth.register)
 
 
 module.exports = router
