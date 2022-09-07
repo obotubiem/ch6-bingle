6fa39fc103cf6c5e7dddff97781279c49669cea0
irvan taufik <irvantaufik28@gmail.com>
2022-09-07 23:56:28 +0700

    add jwt passport

--- src/routes/admin_router.js
+++ src/routes/admin_router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const admin = require("../controllers/admin_controller")
 const handleUpload =require("../libs/handle_Upload")
-
+const passport = require ("passport")
 
 router.post('/product/add',handleUpload.single('photo_product') ,admin.addProduct)
 router.put('/product/update/:id', admin.editProduct)
--- src/routes/product_router.js
+++ src/routes/product_router.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 const product = require('../controllers/product_controller')
 
 
-router.get('/', product.getAllProduct)
+router.get('/',passport.authenticate("jwt", {session: false}) ,product.getAllProduct)
 router.get('/:id', product.getOneProduct)
 
 
